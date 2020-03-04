136cdbcb3579e8a868d05a477be03f06270881a5
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2020-03-04 17:17:29 +0100

    Initiate migration script

---
+++ scripts/dijon/migration/migration_dijon.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+#! python3
+
+"""
+    Name:         Migration script for Dijon data in 2020
+    Author:       Isogeo
+    Purpose:      Script using the migrations-toolbelt package to perform metadata migration.
+                Logs are willingly verbose.
+    Python:       3.7+
+"""
+
+# ##############################################################################
+# ########## Libraries #############
+
+# Standard Library
+import csv
+import logging
+from logging.handlers import RotatingFileHandler
+from os import environ
+from pathlib import Path
+
+# 3rd party
+from dotenv import load_dotenv
+
+# Isogeo
+from isogeo_pysdk import Isogeo, IsogeoChecker
+
+# submodules
+from isogeo_migrations_toolbelt import MetadataDuplicator
+
+# load dijon.env file
+load_dotenv("dijon.env", override=True)
+
+checker = IsogeoChecker()
+
+if __name__ == "__main__":
+    # logs
+    logger = logging.getLogger()
+    # ------------ Log & debug ----------------
+    logging.captureWarnings(True)
+    logger.setLevel(logging.INFO)
+    # logger.setLevel(logging.INFO)
+
+    log_format = logging.Formatter(
+        "%(asctime)s || %(levelname)s "
+        "|| %(module)s - %(lineno)d ||"
+        " %(funcName)s || %(message)s"
+    )
+
+    # debug to the file
+    log_file_handler = RotatingFileHandler(
+        Path("./scripts/dijon/migration/_logs/migration_dijon.log"), "a", 5000000, 1
+    )
+    log_file_handler.setLevel(logging.INFO)
+    log_file_handler.setFormatter(log_format)
+
+    # info to the console
+    log_console_handler = logging.StreamHandler()
+    log_console_handler.setLevel(logging.INFO)
+    log_console_handler.setFormatter(log_format)
+
+    logger.addHandler(log_file_handler)
+    logger.addHandler(log_console_handler)
