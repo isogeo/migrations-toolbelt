31fa60b0d44301826d046d1a1228777695db54d7
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2020-04-28 12:07:39 +0200

    typo

--- scripts/AMP/md_ref/cadastre_geomap/duplicate_trg_cat_into_src_wg.py
+++ scripts/AMP/md_ref/cadastre_geomap/duplicate_trg_cat_into_src_wg.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     logger.info("\n------------ DUPLICATING SESSION ------------")
     # GET CATALOG TO DUPLICATE
     amp_wg_uuid = environ.get("ISOGEO_ORIGIN_WORKGROUP")
-    origin_cat_uuid = environ.get("AMP_CADATRE_CATALOG_UUID")
+    origin_cat_uuid = environ.get("AMP_CADASTRE_CATALOG_UUID")
     dgfip_wg_uuid = environ.get("ISOGEO_DGFIP_WORKGROUP")
     destination_cat_uuid = environ.get("SOURCE_CADASTRE_CATALOG_UUID")
 
