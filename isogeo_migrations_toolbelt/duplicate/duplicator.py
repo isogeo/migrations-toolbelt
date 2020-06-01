a51b92beca7f5970f301763ced7f217fa9aa0f08
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2020-06-01 12:28:56 +0200

    Add links management to duplicate_into_same_group Duplicator method
    #19

--- isogeo_migrations_toolbelt/duplicate/duplicator.py
+++ isogeo_migrations_toolbelt/duplicate/duplicator.py
@@ -247,6 +247,24 @@ class MetadataDuplicator(object):
                 )
             )
 
+        # Links (only URLs)
+        if len(self.metadata_source.links):
+            counter_links = 0
+            for lk in self.metadata_source.links:
+                link = Link(**lk)
+                # ignore hosted links
+                if link.type == "hosted":
+                    logger.info(
+                        "Hosted links can't be migrated, so this link has been ignored: {}".format(
+                            link.title
+                        )
+                    )
+                    continue
+                # add the link
+                self.isogeo.metadata.links.create(metadata=md_dst, link=link)
+                # increase counter
+                counter_links += 1
+
         # Service layers associated
         if self.metadata_source.type in ("rasterDataset", "vectorDataset") and len(
             self.metadata_source.serviceLayers
@@ -607,7 +625,6 @@ class MetadataDuplicator(object):
                 self.isogeo.metadata.links.create(metadata=md_dst, link=link)
                 # increase counter
                 counter_links += 1
-
 
         # Specifications
         if len(self.metadata_source.specifications):
