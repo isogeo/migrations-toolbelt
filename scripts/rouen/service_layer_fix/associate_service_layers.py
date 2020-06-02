4dc17dc8e2f33eeec74e7a4ff05d119b287b3a83
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2020-06-02 18:22:50 +0200

    Initiate script to associate service layers from csv file
    #38

---
+++ scripts/rouen/service_layer_fix/associate_service_layers.py
@@ -0,0 +1,48 @@
+# -*- coding: UTF-8 -*-
+#! python3
+
+"""
+    Name:         Script to associate service layers to metadatas according to a csv file 
+    Author:       Isogeo
+    Purpose:      Script using the migrations-toolbelt package to perform metadata migration.
+                Logs are willingly verbose.
+    Python:       3.7+
+"""
+
+# ##############################################################################
+# ########## Libraries #############
+
+# Standard Library
+import csv
+from os import environ
+from pathlib import Path
+# 3rd party
+from dotenv import load_dotenv
+
+# Isogeo
+from isogeo_pysdk import (
+    Isogeo,
+    IsogeoChecker,
+)
+
+checker = IsogeoChecker()
+# load .env file
+load_dotenv("./env/rouen.env", override=True)
+
+if __name__ == "__main__":
+
+    # ############################### MIGRATING & SAVING ###############################
+    # API client instanciation
+    isogeo = Isogeo(
+        client_id=environ.get("ISOGEO_API_USER_LEGACY_CLIENT_ID"),
+        client_secret=environ.get("ISOGEO_API_USER_LEGACY_CLIENT_SECRET"),
+        auth_mode="user_legacy",
+        auto_refresh_url="{}/oauth/token".format(environ.get("ISOGEO_ID_URL")),
+        platform=environ.get("ISOGEO_PLATFORM", "qa"),
+    )
+    isogeo.connect(
+        username=environ.get("ISOGEO_USER_NAME"),
+        password=environ.get("ISOGEO_USER_PASSWORD"),
+    )
+
+    service_md_uuid = tuple(environ.get("NEW_SERVICE_MD_UUIDS").split(";"))
