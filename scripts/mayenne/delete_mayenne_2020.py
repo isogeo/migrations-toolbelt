dca6d87ae6be850dd42f98feee08dc6238739f7b
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2020-06-29 12:34:21 +0200

    Finalize Mayenne deletion script
    #29

--- scripts/mayenne/delete_mayenne_2020.py
+++ scripts/mayenne/delete_mayenne_2020.py
@@ -19,21 +19,20 @@ from os import environ
 from pathlib import Path
 from time import sleep
 from timeit import default_timer
+import csv
 
 # 3rd party
 from dotenv import load_dotenv
 
 # Isogeo
-from isogeo_pysdk import Isogeo, IsogeoChecker, Metadata
+from isogeo_pysdk import Isogeo
 
 # submodules
-from isogeo_migrations_toolbelt import BackupManager
+from isogeo_migrations_toolbelt import BackupManager, MetadataDeleter
 
 
 # load .env file
 load_dotenv("./env/mayenne.env", override=True)
-
-checker = IsogeoChecker()
 
 if __name__ == "__main__":
     # logs
@@ -64,10 +63,35 @@ if __name__ == "__main__":
     logger.addHandler(log_file_handler)
     logger.addHandler(log_console_handler)
 
-    src_cat = environ.get("ISOGEO_CATALOG_SOURCE")
-    trg_cat = environ.get("ISOGEO_CATALOG_TARGET")
+    # ################# RETRIEVE THE LIST OF SRC MD'S UUID TO DELETE FROM CSV FILE #######################
+    li_md_to_delete = []
+    # prepare csv reading
+    input_csv = Path(r"./scripts/mayenne/csv/to_delete.csv")
+    fieldnames = [
+        "source_uuid",
+        "source_title",
+        "source_name",
+        "target_name",
+        "target_uuid"
+    ]
+    with input_csv.open() as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";", fieldnames=fieldnames)
 
-    # ################# MAKE THE LIST OF SRC MD'S UUID TO DELETE #######################
+        row_num = 0
+        for row in reader:
+            row_num += 1
+            src_uuid = row.get("source_uuid")
+            src_title = row.get("source_title")
+            src_name = row.get("source_name")
+            trg_name = row.get("target_name")
+            trg_uuid = row.get("target_uuid")
+            if src_uuid != "source_uuid":
+                # check if the target metadata exists
+                li_md_to_delete.append(src_uuid)
+            else:
+                pass
+
+    # ################## BACKUP THEN DELETE METADATAS RETRIEVED ##################
     # API client instanciation
     isogeo = Isogeo(
         client_id=environ.get("ISOGEO_API_USER_LEGACY_CLIENT_ID"),
@@ -82,75 +106,48 @@ if __name__ == "__main__":
     )
     auth_timer = default_timer()
 
-    src_md = isogeo.search(
-        group=environ.get("ISOGEO_ORIGIN_WORKGROUP"),
-        whole_results=True,
-        query="catalog:{}".format(src_cat),
-        include="all"
-    )
-
-    # listing
-    li_md_to_delete = []
-    for md in src_md.results:
-        metadata = Metadata.clean_attributes(md)
-        md_cat = [metadata.tags.get(tag) for tag in metadata.tags if tag.startswith("catalog:")]
-        if trg_cat not in md_cat:
-            li_md_to_delete.append(metadata._id)
-        else:
-            pass
     logger.info("------- {} source metadatas listed gonna be backuped then deleted -------".format(len(li_md_to_delete)))
     # ################# BACKUP MDs THAT ARE GONNA BE DELETED #######################
-    # instanciate backup manager
-    backup_path = Path(r"./scripts/mayenne/_output/_backup_deleted")
-    backup_mng = BackupManager(api_client=isogeo, output_folder=backup_path)
 
-    # lauching backup
-    amplitude = 50
-    bound_range = int(len(li_md_to_delete) / amplitude)
-    li_bound = []
-    for i in range(bound_range + 1):
-        li_bound.append(amplitude * i)
-    li_bound.append(len(li_md_to_delete))
+    # ------------------------------------ BACKUP --------------------------------------
+    if environ.get("BACKUP") == "1" and len(li_md_to_delete):
+        logger.info("---------------------------- BACKUP ---------------------------------")
+        # instanciate backup manager
+        backup_path = Path(r"./scripts/mayenne/_output/_backup_deleted")
+        backup_mng = BackupManager(api_client=isogeo, output_folder=backup_path)
+        # lauching backup
+        amplitude = 50
+        bound_range = int(len(li_md_to_delete) / amplitude)
+        li_bound = []
+        for i in range(bound_range + 1):
+            li_bound.append(amplitude * i)
+        li_bound.append(len(li_md_to_delete))
 
-    logger.info("------- Starting backup for {} rounds -------".format(len(li_bound) - 1))
-    for i in range(len(li_bound) - 1):
-        if default_timer() - auth_timer >= 250:
-            logger.info("Manually refreshing token")
-            backup_mng.isogeo.connect(
-                username=environ.get("ISOGEO_USER_NAME"),
-                password=environ.get("ISOGEO_USER_PASSWORD"),
-            )
-            auth_timer = default_timer()
-        else:
-            pass
-        bound_inf = li_bound[i]
-        bound_sup = li_bound[i + 1]
-        logger.info("Round {} - backup from source metadata {} to {}".format(i + 1, bound_inf + 1, bound_sup))
+        logger.info("------- Starting backup for {} rounds -------".format(len(li_bound) - 1))
+        for i in range(len(li_bound) - 1):
+            if default_timer() - auth_timer >= 250:
+                logger.info("Manually refreshing token")
+                backup_mng.isogeo.connect(
+                    username=environ.get("ISOGEO_USER_NAME"),
+                    password=environ.get("ISOGEO_USER_PASSWORD"),
+                )
+                auth_timer = default_timer()
+            else:
+                pass
+            bound_inf = li_bound[i]
+            bound_sup = li_bound[i + 1]
+            logger.info("Round {} - backup from source metadata {} to {}".format(i + 1, bound_inf + 1, bound_sup))
 
-        search_parameters = {"query": None, "specific_md": tuple(li_md_to_delete[bound_inf:bound_sup])}
-        try:
-            backup_mng.metadata(search_params=search_parameters)
-        except Exception as e:
-            logger.info("an error occured : {}".format(e))
+            search_parameters = {"query": None, "specific_md": tuple(li_md_to_delete[bound_inf:bound_sup])}
+            try:
+                backup_mng.metadata(search_params=search_parameters)
+            except Exception as e:
+                logger.info("an error occured : {}".format(e))
 
     # ################# DELETE LISTED SRC MDs #######################
     logger.info("------- Starting to delete {} source metadatas -------".format(len(li_md_to_delete)))
-    for md in li_md_to_delete:
-        count = li_md_to_delete.index(md) + 1
-        if (count % 50) == 0:
-            logger.info("From {} to {} metadatas deleted ".format(count - 50, count))
-            sleep(2)
-        else:
-            pass
-        if default_timer() - auth_timer >= 250:
-            logger.info("Manually refreshing token")
-            isogeo.connect(
-                username=environ.get("ISOGEO_USER_NAME"),
-                password=environ.get("ISOGEO_USER_PASSWORD"),
-            )
-            auth_timer = default_timer()
-        else:
-            pass
-        isogeo.metadata.delete(md)
+
+    md_dltr = MetadataDeleter(api_client=isogeo)
+    md_dltr.metadata(li_md_to_delete)
 
     isogeo.close()
